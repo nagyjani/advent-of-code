d229a8308d6abc7046accdda66f5a3e24028b32a
Janos Gabor Nagy <janos-gabor.nagy@klarna.com>
2022-12-02 07:15:02 +0100

    Add the first two solutions, errors with intellij to be fixed

---
+++ src/2022/Day01.kt
@@ -0,0 +1,49 @@
+import java.io.File
+import java.util.*
+
+fun main() {
+    Day01().solve()
+}
+
+
+class Day01 {
+
+    val input1 = """
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+    """.trimIndent()
+
+
+    fun solve() {
+        val f = File("/home/janos/Downloads/aoc2201.in")
+        val s = Scanner(f)
+//                val s = Scanner(input1)
+        val cals = mutableListOf<MutableList<Int>>(mutableListOf())
+        while (s.hasNextLine()) {
+            val line = s.nextLine().trim()
+            if (line.isEmpty()) {
+                cals.add(mutableListOf())
+                continue
+            }
+            cals.last().add(line.toInt())
+        }
+
+        val maxCals = cals.maxOf { it.sum() }
+
+        val sumCals = cals.map { it.sum() }.sortedDescending()
+
+        print("$maxCals ${sumCals[0]+sumCals[1]+sumCals[2]}\n")
+    }
+}
---
+++ src/2022/Day02.kt
@@ -0,0 +1,56 @@
+import java.io.File
+import java.util.*
+
+fun main() {
+    Day02().solve()
+}
+
+
+class Day02 {
+
+    val input1 = """
+A Y
+B X
+C Z
+    """.trimIndent()
+
+    fun points(r: Pair<Int, Int>): Int {
+        var p = r.second + 1
+        if (r.first == r.second) {
+            p += 3
+        } else if ((r.first+1)%3 == r.second) {
+            p += 6
+        }
+//        print("$r $p\n")
+        return p
+    }
+
+    fun points2(r: Pair<Int, Int>): Int {
+        // 0: lose 1: draw 2: win
+        val other = r.first
+        var own = other
+        if (r.second == 0) {
+            own = (other+2)%3
+        } else if (r.second == 2) {
+            own = (other+1)%3
+        }
+        return points(Pair(other, own))
+    }
+
+    fun solve() {
+        val f = File("/home/janos/Downloads/aoc2202.in")
+        val s = Scanner(f)
+//                val s = Scanner(input1)
+        val matches = mutableListOf<Pair<Int, Int>>()
+        while (s.hasNext()) {
+            val other = s.next().trim()[0] - 'A'
+            val own = s.next().trim()[0] - 'X'
+            matches.add(Pair(other, own))
+        }
+
+        val sumPoints = matches.sumOf { points(it) }
+        val sumPoints2 = matches.sumOf { points2(it) }
+
+        print("$sumPoints $sumPoints2\n")
+    }
+}
