7314736b8fb4d4d779944a93e0648061c7ef1ae6
janos-gabor.nagy <janos-gabor.nagy@klarna.com>
2023-12-13 06:41:11 +0100

    day 13

--- src/2023/Day13.kt
+++ src/2023/Day13.kt
@@ -2,6 +2,7 @@ package `2023`
 
 
 import java.io.File
+import java.lang.StringBuilder
 import java.util.*
 
 fun main() {
@@ -12,29 +13,102 @@ fun main() {
 class Day13 {
 
     val input1 = """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
         
+                #...##..#
+                #....#..#
+                ..##..###
+                #####.##.
+                #####.##.
+                ..##..###
+                #....#..#
     """.trimIndent()
 
     val input2 = """
         
+
+
     """.trimIndent()
+    
+    fun List<String>.transpose(): List<String> {
+        val r = List(this[0].length){StringBuilder()}
+        forEachIndexed{ ix1, it1 -> it1.forEachIndexed{ ix2, it2 -> r[ix2].append(it2) } }
+        return r.map { it.toString() }
+    }
+
+
+    fun List<String>.vMirror(d: Int): Int {
+        return transpose().hMirror(d)
+    }
+
+    fun List<String>.hMirror(d: Int): Int {
+        var r = 0
+        l@ for (i in 0 until size-1) {
+            r = 0
+            for (j in 0 .. i) {
+                if (i+1+j >= size) {
+                    continue@l
+                }
+                val s1 = get(i-j)
+                val s2 = get(i+1+j)
+                s1.indices.forEach{if (s1[it] != s2[it]) {++r} }
+                if (r > d) {
+                    continue@l
+                }
+                if ((j == i || i+1+j == size-1) && r==d) {
+                    return i+1
+                }
+            }
+        }
+        return -1
+    }
+
+    fun List<String>.mirror(d: Int = 0): Int {
+        var m = hMirror(d)
+        if (m == -1) {
+            return vMirror(d)
+        }
+        return m*100
+    }
 
     fun solve() {
-//        val f = File("src/2023/inputs/day13.in")
-//        val s = Scanner(f)
-        val s = Scanner(input1)
+        val f = File("src/2023/inputs/day13.in")
+        val s = Scanner(f)
+//        val s = Scanner(input1)
 //        val s = Scanner(input2)
         var sum = 0
         var sum1 = 0
         var lineix = 0
         val lines = mutableListOf<String>()
+        val maps = mutableListOf<MutableList<String>>(mutableListOf<String>())
         while (s.hasNextLine()) {
             lineix++
             val line = s.nextLine().trim()
-            if (line.isEmpty()) {
+            if (line.isEmpty() && maps.last().isNotEmpty()) {
+                maps.add(mutableListOf())
                 continue
             }
             lines.add(line)
+            maps.last().add(line)
+        }
+        
+        if (maps.last().isEmpty()) {
+            maps.removeAt(maps.size-1)
+        }
+
+        var e = 0
+        for (m in maps) {
+            val r = m.mirror()
+            val r1 = m.mirror(1)
+            println("$e $r $r1")
+            sum += r
+            sum1 += r1
         }
 
         print("$sum $sum1\n")
---
+++ src/2023/Day14.kt
@@ -0,0 +1,42 @@
+package `2023`
+
+
+import java.io.File
+import java.util.*
+
+fun main() {
+    Day14().solve()
+}
+
+
+class Day14 {
+
+    val input1 = """
+        
+    """.trimIndent()
+
+    val input2 = """
+        
+    """.trimIndent()
+
+    fun solve() {
+//        val f = File("src/2023/inputs/day14.in")
+//        val s = Scanner(f)
+        val s = Scanner(input1)
+//        val s = Scanner(input2)
+        var sum = 0
+        var sum1 = 0
+        var lineix = 0
+        val lines = mutableListOf<String>()
+        while (s.hasNextLine()) {
+            lineix++
+            val line = s.nextLine().trim()
+            if (line.isEmpty()) {
+                continue
+            }
+            lines.add(line)
+        }
+
+        print("$sum $sum1\n")
+    }
+}
