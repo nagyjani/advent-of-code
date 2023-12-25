f0c61c787e4f0b83b69ed0cde3117aed3ae918a5
janos-gabor.nagy <janos-gabor.nagy@klarna.com>
2023-12-25 11:24:12 +0100

    day 24 and 25

--- src/2023/Day24.kt
+++ src/2023/Day24.kt
@@ -1,8 +1,12 @@
 package `2023`
 
 
+import `2021`.sign
 import java.io.File
+import java.lang.RuntimeException
+import java.math.BigInteger
 import java.util.*
+import kotlin.math.sign
 
 fun main() {
     Day24().solve()
@@ -13,21 +17,65 @@ class Day24 {
 
     val input1 = """
         
+
+        19, 13, 30 @ -2,  1, -2
+        18, 19, 22 @ -1, -1, -2
+        20, 25, 34 @ -2, -2, -4
+        12, 31, 28 @ -1, -2, -1
+        20, 19, 15 @  1, -5, -3
+
     """.trimIndent()
 
     val input2 = """
-        
     """.trimIndent()
 
+
+    class Hail3D(val p: List<Long>, val v: List<Long>)
+
+    class Hail2D(val x0: Long, val y0: Long, val vx: Long, val vy: Long) {
+        fun collides(other: Hail2D, min: Double, max: Double): Boolean {
+
+
+            val a = vy.toDouble() / vx.toDouble()
+            val b = (y0*vx - x0*vy).toDouble() / vx.toDouble()
+            val a1 = other.vy.toDouble() / other.vx.toDouble()
+            val b1 = (other.y0*other.vx - other.x0*other.vy).toDouble() / other.vx.toDouble()
+            val x = (b - b1) / (a1 - a)
+            val y = a * x + b
+            val t = (x-x0.toDouble()) / vx.toDouble()
+            val t1 = (x-other.x0.toDouble()) / other.vx.toDouble()
+
+            if (min > x || min > y || max < x || max < y || t < 0 || t1 < 0) {
+                return false
+            }
+            return true
+        }
+
+    }
+
+    fun String.toHail2D(): Hail2D {
+        val (x0, y0, vx, vy) = Regex("(\\w+), +(\\w+), +.+@ +([-\\d]+), +([-\\d]+)").find(this)!!.destructured
+        return Hail2D(x0.toLong(), y0.toLong(), vx.toLong(), vy.toLong())
+    }
+
+    fun String.toHail3D(): Hail3D {
+        val (x0, y0, z0, vx, vy, vz) = Regex("(\\w+), +(\\w+), +(\\w+).*@ +([-\\d]+), +([-\\d]+), +([-\\d]+)").find(this)!!.destructured
+        return Hail3D(listOf(x0.toLong(), y0.toLong(), z0.toLong()), listOf(vx.toLong(), vy.toLong(), vz.toLong()))
+    }
+
     fun solve() {
-//        val f = File("src/2023/inputs/day24.in")
-//        val s = Scanner(f)
-        val s = Scanner(input1)
+        val f = File("src/2023/inputs/day24.in")
+        val s = Scanner(f)
+//        val s = Scanner(input1)
 //        val s = Scanner(input2)
         var sum = 0
         var sum1 = 0
         var lineix = 0
         val lines = mutableListOf<String>()
+        val hails2d = mutableListOf<Hail2D>()
+        val hails3d = mutableListOf<Hail3D>()
+        val same = listOf(mutableMapOf<Int, Int>(),mutableMapOf<Int, Int>(),mutableMapOf<Int, Int>())
+
         while (s.hasNextLine()) {
             lineix++
             val line = s.nextLine().trim()
@@ -35,8 +83,95 @@ class Day24 {
                 continue
             }
             lines.add(line)
+            hails2d.add(line.toHail2D())
+            hails3d.add(line.toHail3D())
+            (0..2).forEach{same[it][hails3d.last().v[it].toInt()] = same[it].getOrDefault(hails3d.last().v[it].toInt(), 0) + 1}
+        }
+
+        val ts = listOf(0 to 21, 0 to -88, 0 to -154, 0 to 63, 1 to -18, 1 to -36, 1 to -12, 1 to -81,
+        2 to 8, 2 to -59, 2 to -108, 2 to -15)
+        val vts = mutableMapOf<Int, Set<Int>>()
+        for (t in ts) {
+            val (ix, value) = t
+            val hs = hails3d.filter { it.v[ix] == value.toLong() }
+            if (hs.size < 2) {
+                continue
+            }
+            val ns = mutableSetOf<Int>()
+            for (n in 1..2000) {
+                val a = hs[0].p[ix] % n
+                if (hs.all { it.p[ix] % n == a }) {
+                    ns.add(value-n)
+                    ns.add(value+n)
+                }
+            }
+            if (vts.contains(ix)) {
+                vts[ix] = ns.intersect(vts[ix]!!)
+            } else {
+                vts[ix] = ns
+            }
+//            println(ns)
+        }
+
+        val vx = -153
+        val vy = -150
+        val vz = 296
+
+        val dvx0 = BigInteger.valueOf(vx-hails3d[0].v[0])
+        val dvy0 = BigInteger.valueOf(vy-hails3d[0].v[1])
+        val dvz0 = BigInteger.valueOf(vz-hails3d[0].v[2])
+        val dvx1 = BigInteger.valueOf(vx-hails3d[1].v[0])
+        val dvy1 = BigInteger.valueOf(vy-hails3d[1].v[1])
+        val x0 = BigInteger.valueOf(hails3d[0].p[0])
+        val y0 = BigInteger.valueOf(hails3d[0].p[1])
+        val z0 = BigInteger.valueOf(hails3d[0].p[2])
+        val x1 = BigInteger.valueOf(hails3d[1].p[0])
+        val y1 = BigInteger.valueOf(hails3d[1].p[1])
+
+        val yn = (x0 * dvy0 * dvy1 - y0 * dvx0 * dvy1 - x1 * dvy1 * dvy0 + y1 * dvx1 * dvy0)
+        val yd = (dvx1 * dvy0 - dvx0 * dvy1)
+        val yd1 = BigInteger.valueOf(vx-hails3d[2].v[0]) * BigInteger.valueOf(vy-hails3d[0].v[1]) -
+                BigInteger.valueOf(vx-hails3d[2].v[1]) * BigInteger.valueOf(vy-hails3d[0].v[0])
+
+        val v = listOf(-153, -150, 296)
+        val min = mutableListOf<BigInteger?>(null, null, null)
+        val max = mutableListOf<BigInteger?>(null, null, null)
+
+        hails3d.forEach {
+            h ->
+            (0..2). forEach {
+                ix ->
+                val dv = v[ix] - h.v[ix]
+                if (dv < 0 && (min[ix] == null || min[ix]!! < BigInteger.valueOf(h.p[ix]))) {
+                    min[ix] = BigInteger.valueOf(h.p[ix])
+                }
+                if (dv > 0 && (max[ix] == null || max[ix]!! > BigInteger.valueOf(h.p[ix]))) {
+                    max[ix] = BigInteger.valueOf(h.p[ix])
+                }
+            }
+        }
+
+        val m = yn % yd
+        val y = yn / yd
+
+        val t0 = (y0 - y)/dvy0
+        val tm0 = (y0 - y)%dvy0
+
+        val x = x0 - t0 * dvx0
+        val z = z0 - t0 * dvz0
+
+        for (i in 0 until hails2d.size) {
+            for (j in i+1 until hails2d.size) {
+                if (hails2d[i].collides(hails2d[j], 7.toDouble(), 27.toDouble())) {
+                    sum ++
+                }
+                if (hails2d[i].collides(hails2d[j], 200000000000000.toDouble(), 400000000000000.toDouble())) {
+                    sum1 ++
+                }
+            }
         }
 
         print("$sum $sum1\n")
+        println(x + y + z)
     }
 }
--- src/2023/Day25.kt
+++ src/2023/Day25.kt
@@ -12,22 +12,100 @@ fun main() {
 class Day25 {
 
     val input1 = """
-        
+        jqt: rhn xhk nvd
+        rsh: frs pzl lsr
+        xhk: hfx
+        cmg: qnr nvd lhk bvb
+        rhn: xhk bvb hfx
+        bvb: xhk hfx
+        pzl: lsr hfx nvd
+        qnr: nvd
+        ntq: jqt hfx bvb xhk
+        nvd: lhk
+        lsr: lhk
+        rzs: qnr cmg lsr rsh
+        frs: qnr lhk lsr
     """.trimIndent()
 
     val input2 = """
         
     """.trimIndent()
 
+    // for one node
+    // the shortest path to another (~1500)
+    // remove one node from the path (K)
+    // find the shortest again (3)
+
+
+    fun Map<String, Set<String>>.findPath(source: String, target: String, removed: Set<Pair<String, String>>): List<String>? {
+        return findPath(target, listOf(source), removed)
+    }
+
+    fun Map<String, Set<String>>.findPath(target: String, path: List<String>, removed: Set<Pair<String, String>>): List<String>? {
+        val nextNodes = get(path.last())!!
+        if (nextNodes.contains(target)) {
+            return path.toMutableList().apply {  add(target)}
+        }
+        for (n in nextNodes) {
+            if (path.contains(n)) {
+                continue
+            }
+            if (removed.contains(path.last() to n)) {
+                continue
+            }
+            val r = findPath(target, path.toMutableList().apply { add(n) }, removed)
+            if (r == null) {
+                continue
+            }
+            return r
+        }
+        return null
+    }
+
+    fun Map<String, Set<String>>.findPath2(source: String, target: String, removed: Set<Pair<String, String>>): Pair<List<Pair<String, String>>?,Int> {
+        val paths = mutableMapOf<String, String>()
+        val toCheck = get(source)!!.map { it to source }.filter{!removed.contains(it) && !removed.contains(it.second to it.first)}.toMap().toMutableMap()
+        if (removed.size == 3 && (removed.contains("nvd" to "jqt") || removed.contains("jqt" to "nvd")) && (removed.contains("pzl" to "hfx") || removed.contains("hfx" to "pzl")) && (removed.contains("cmg" to "bvb") || removed.contains("bvb" to "cmg"))
+        ) {
+            Unit
+        }
+        while (toCheck.isNotEmpty()) {
+            val n = toCheck.keys.first()
+            val s = toCheck[n]!!
+            toCheck.remove(n)
+            if (paths.contains(n)) {
+                continue
+            }
+            paths[n] = s
+            get(n)!!.filter { it != source && !paths.contains(it) && !removed.contains(it to n) && !removed.contains(n to it) }.forEach { toCheck[it] = n }
+        }
+
+        if (paths.contains(target)) {
+            val r = mutableListOf<Pair<String,String>>()
+            var n = target
+            while (n != source) {
+                r.add(paths[n]!! to n)
+                n = paths[n]!!
+            }
+            return r.reversed() to paths.size
+        }
+
+        return null to paths.size
+    }
+
+
+
+
     fun solve() {
-//        val f = File("src/2023/inputs/day25.in")
-//        val s = Scanner(f)
-        val s = Scanner(input1)
+        val f = File("src/2023/inputs/day25.in")
+        val s = Scanner(f)
+//        val s = Scanner(input1)
 //        val s = Scanner(input2)
         var sum = 0
         var sum1 = 0
         var lineix = 0
         val lines = mutableListOf<String>()
+        val nodes = mutableMapOf<String, MutableSet<String>>()
         while (s.hasNextLine()) {
             lineix++
             val line = s.nextLine().trim()
@@ -35,6 +113,57 @@ class Day25 {
                 continue
             }
             lines.add(line)
+            val t1 = line.split(Regex(": "))
+            val node = t1[0]
+            t1[1].split(" ").forEach {
+                if (nodes.contains(node)) {
+                    nodes[node]!!.add(it)
+                } else {
+                    nodes[node] = mutableSetOf(it)
+                }
+                if (nodes.contains(it)) {
+                    nodes[it]!!.add(node)
+                } else {
+                    nodes[it] = mutableSetOf(node)
+                }
+            }
+        }
+
+        val e = nodes.findPath2(nodes.keys.first(), nodes.keys.last(), setOf())
+
+        val source = nodes.keys.first()
+        val targets = nodes.keys.toMutableList().apply{remove(source)}
+
+        main@ for (t in targets) {
+            val (d, _) = nodes.findPath2(source, t, setOf())
+            for (removed1 in d!!) {
+                val (d1, _) = nodes.findPath2(source, t, setOf(removed1))
+                if (t == "nvd" && (removed1 == "nvd" to "jqt" || removed1 == "jqt" to "nvd")) {
+                    Unit
+                }
+                if (d1 == null) {
+                    println("1: $removed1")
+                    continue
+                }
+                for (removed2 in d1!!) {
+                    val (d2, _) = nodes.findPath2(source, t, setOf(removed1, removed2))
+                    if (t == "nvd" && (removed1 == "nvd" to "jqt" || removed1 == "jqt" to "nvd") && (removed2 == "pzl" to "hfx" || removed2 == "hfx" to "pzl")) {
+                        Unit
+                    }
+                    if (d2 == null) {
+                        println("2: $removed1 $removed2")
+                        continue
+                    }
+                    for (removed3 in d2!!) {
+                        val (d3, s3) = nodes.findPath2(source, t, setOf(removed1, removed2, removed3))
+                        if (d3 == null) {
+                            val d4 = nodes.findPath2(source, t, setOf(removed1, removed2, removed3))
+                            println("3: $removed1 $removed2 $removed3 ($s3) (${(s3 + 1) * (nodes.size - s3 -1)})")
+                            break@main
+                        }
+                    }
+                }
+            }
         }
 
         print("$sum $sum1\n")
